0b5d9d68d60c50d8a1aa1efeed9289ed18aeada4
LL Studio <ll-studio@LLs-MacBook-Pro.local>
2022-08-22 12:56:32 -0400

    Updates:

---
+++ pages/api/lyrics-with-sentiment-formatted.js
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const lyricsFinder = require('lyrics-finder');
+var Sentiment = require('sentiment');
+
+
+export default async function lyrics(req, res) {
+    if (req.query && req.query.artist && req.query.song) {
+        console.log(JSON.stringify(req.query))
+        let lyrics = await lyricsFinder(req.query.artist, req.query.song) || "Not Found!"; 
+        var sentiment = new Sentiment();
+        var result = sentiment.analyze(lyrics)
+        // res.status(200).json({ artist: req.query.artist, song: req.query.song, lyrics: lyrics, sentiment: result })
+        res.status(200).send(`<h1>result: ${result.comparative}</h1><pre>${lyrics}</pre><pre>${JSON.stringify({ artist: req.query.artist, song: req.query.song, lyrics: lyrics, sentiment: result }, null, 4)}</pre>`)
+    }
+    
+  }
+  
